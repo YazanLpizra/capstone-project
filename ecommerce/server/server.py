9d61b8d5f6ce08a26be7fa4d704caa48e40a3cc4
Yazan Lpizra <ylpizra@my.bridgeport.edu>
2024-05-05 17:13:24 -0400

    wrapped it up

--- ecommerce/server/server.py
+++ ecommerce/server/server.py
@@ -1,22 +1,40 @@
-from flask import Flask
-import json
+from flask import Flask, render_template, request, flash
+from db_controller import DB_Controller
 
 app = Flask(__name__)
 
-connection = mariadb.connect(
-    user="admin",
-    password="admin",
-    database='ecommerce',
-    host='localhost:'
-)
 
-@app.route("/products/<category>")
-def get_product_in_category(category):
-    cursor = connection.cursor()
-    if category == 'any':
-        products = cursor.excecute('select * from products where product_category = %s', (category,))
-    else:
-        products = cursor.excecute('select * from products ', (category,))
-    cursor.close()
+@app.get("/products/category/<category>")
+def GET__products_partial(category):
+    return render_template(
+        "products.html", 
+        products = DB_Controller.get_products_by_category(category)
+    )
 
-    return json.dumps(products)
+@app.get("/products/review/<int:id>")
+def GET__product_review_form_partial(id):
+    product = DB_Controller.get_product_by_id(id)
+    return render_template(
+        "product_review_popup.html",
+        product=product
+    )
+
+
+@app.post("/products/review/<int:id>")
+def POST__product_review(id):
+    print(request.is_json)
+    print(request.get_json())
+    print(request.json)
+    print(request.form)
+    product_review = request.get_json()["product_review"]
+    DB_Controller.create_product_review(id, product_review)
+    return {"isSuccessful":True}
+
+
+
+@app.get("/")
+def GET__home_page():
+    return render_template(
+        "main.html", 
+        products = DB_Controller.get_products_by_category('any')
+    )
--- notes.md
+++ notes.md
@@ -1,6 +1,16 @@
-## Step 1: create env
+## Step 1: install XAMPP and run mysql on port 3306
+## Step 2: create python env with deps
 ```
-conda create --name capstone python=3.10
-conda install flask
-
+    conda create --name capstone python=3.10
+    conda activate capstone
+    conda install flask conda-forge::mysql-connector-python
+```
+## Step 3: initialize database and populate with products
+```
+    cd ecommerce\server
+    python .\db-init\db-setup.py
+```
+## Step 4: run application
+```
+    flask --app server run
 ```
